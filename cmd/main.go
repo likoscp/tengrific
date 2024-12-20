433f5a648dd91cd6fb7789a147d267f997500045
likoscp <Abdikhalikova_sabina@protonmail.com>
2024-12-20 11:14:17 +0500

    localhost

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,6 @@ func main() {
 	config.LoadEnv()
 	db.ConnectDB()
 	router := routes.InitRoutes()
-	log.Println("Server running on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", router))
+	log.Println("Server running on http://127.0.0.1:8080")
+	log.Fatal(http.ListenAndServe("127.0.0.1:8080", router))
 }
